8ab79e936420fb8441c58fcc6af5356080a68e81
Sajid <yeamimhossainsajid@gmail.com>
2025-06-03 22:20:55 +0600

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Experience from "./components/Experience";
-import SEO from "./components/Seo";
+import SEO from "./components/SEO";
 
 function App() {
   
